81148252e10795b31939864c008a21ee410590c8
sathish7291 <sathish010395@gmail.com>
2024-08-17 12:17:41 +0530

    react-usecontext

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import CardContextComponent from "./utils/CardContextComponent";
+import { Navigate } from "react-router-dom";
+import PurchaseContextComponent from "./utils/PurchaseContextComponent";
+import AddToCard from "./Components/AddToCard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <CardContextComponent>
+          <PurchaseContextComponent>
+            <Routes>
+              <Route  path="cart" element={<PurchaseContextComponent> <AddToCard /> </PurchaseContextComponent> }/>
+              <Route path="*" element={<Navigate to="cart" />}/>
+            </Routes>
+          </PurchaseContextComponent>
+        </CardContextComponent>    
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
